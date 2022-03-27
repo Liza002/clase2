014ad101c7084c9d5295e762c291a2b5104ac6e8
Liza002 <101838186+Liza002@users.noreply.github.com>
2022-03-26 23:54:26 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+
+
+function escribirTablaMultiplicar(j){
+
+	document.write("<h2> Tabla del "+ j +"</h2>");
+
+	document.write("<ul>");
+	//i++ significa i=i+1;
+
+	for(i = 1;i<=10;i++){
+		document.write("<li>");
+		document.write(j + "x " + i + "= " + j * i);
+		document.write("</li>");
+	}
+
+	document.write("</ul>");
+}
+
+escribirTablaMultiplicar(5);
+
+
+
+
+function escribirTablaMultiplicar(j){
+
+	document.write("<h2> Tabla del "+ j +"</h2>");
+
+	document.write("<ul>");
+	//i++ significa i=i+1;
+
+	for(i = 1;i<=10;i++){
+		document.write("<li>");
+		document.write(j + "x " + i + "= " + j * i);
+		document.write("</li>");
+	}
+
+	document.write("</ul>");
+}
+
+escribirTablaMultiplicar(6);
